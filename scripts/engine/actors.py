e2b040efc98a9855f2d1b1e158aaf5dccb8b9808
Jackson <jacksonrgoodman@gmail.com>
2022-01-27 00:08:47 -0600

    refactored ideas

--- scripts/engine/actors.py
+++ scripts/engine/actors.py
@@ -8,3 +8,26 @@ class Player:
         self.won = False
         self.status_effects = []
         self.location = ""
+        self.items = []
+
+
+# csv ideas
+# ? def player_index(player):(
+#   savePlayer = Player()
+#   savePlayer.name = player.name
+#   savePlayer.hp = player.hp
+#   savePlayer.ap = player.ap
+#   savePlayer.pp =player.pp
+#   savePlayer.job = player.job
+#   savePlayer.won = player.won
+#   savePlayer.status_effects = player.status_effects
+#   savePlayer.location = player.location
+#   savePlayer.items = player.items
+# )
+# ?
+# ?
+# ?
+# ?
+# ?
+
+# csv prints...('('jacks',0,0,0,'mage',false,[],'d4',[])')
